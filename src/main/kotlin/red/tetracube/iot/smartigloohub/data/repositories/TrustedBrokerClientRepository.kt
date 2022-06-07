1715680e63f79bffcef785f1e0f85d49228719e0
Dave.Di.Do <davide.dcube@gmail.com>
2022-06-07 23:20:25 +0200

    🏔️ Created repositories stubs

---
+++ src/main/kotlin/red/tetracube/iot/smartigloohub/data/repositories/TrustedBrokerClientRepository.kt
@@ -0,0 +1,10 @@
+package red.tetracube.iot.smartigloohub.data.repositories
+
+import org.hibernate.reactive.mutiny.Mutiny
+import javax.enterprise.context.ApplicationScoped
+
+@ApplicationScoped
+class TrustedBrokerClientRepository(
+    private val rxSessionFactory: Mutiny.SessionFactory
+) {
+}
